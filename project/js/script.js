c13ae9988d307a6f736748ae9cabb947a0b2df11
Michael <mix.zhuckow2018@yandex.ru>
2022-06-23 17:40:19 +0300

    Это наш первый комит

---
+++ project/js/script.js
@@ -0,0 +1,34 @@
+`use strict`;
+let persone = 
+{
+    name: "John", 
+    age: 25,
+    isMarried: false 
+};
+
+console.log(persone["name"]);
+
+let arr = [`plum.png`,`orange.jpg`,`apple.bmp`];
+
+console.log(arr[2]);
+
+// alert("Привет долбаеб, снова ты")
+
+// let answer = confirm("Are you here?")
+
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "ДА");
+
+// console.log(answer);
+
+// console.log("arr" + " - object");
+// console.log( 4 + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+alert("ну и че");
+// console.log(++incr);
+// console.log(--decr);
+
